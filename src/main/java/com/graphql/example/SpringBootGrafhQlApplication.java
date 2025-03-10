cdc23505715168dd63f79bc4401b6c8bb893e236
r.mutt22 <matiaszelarayan87@gmail.com>
2025-03-10 12:36:24 -0300

    firts commit

---
+++ src/main/java/com/graphql/example/SpringBootGrafhQlApplication.java
@@ -0,0 +1,13 @@
+package com.graphql.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGrafhQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGrafhQlApplication.class, args);
+	}
+
+}
